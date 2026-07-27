6990566597dfcfae173d4beeb2b02c79b30aea43
Nadine <trauflernadine@gmail.com>
2026-07-27 09:32:28 +0200

    .gitattributes to change sql files detection

--- top-sql-50/05-Advanced-Select-and-Joins/02-primary-department-for-each-employee.sql
+++ top-sql-50/05-Advanced-Select-and-Joins/02-primary-department-for-each-employee.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 With DepartementCount AS (
     SELECT employee_id, COUNT(department_id) as dpt_count FROM Employee 
     GROUP BY employee_id
