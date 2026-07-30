7e5ed4412b3fa6ee5d8623da60040d6e62555625
Nadine <trauflernadine@gmail.com>
2026-07-30 20:38:27 +0200

    SQL / capital-gain-loss

---
+++ Databases/1393-capital-gain-loss.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+SELECT stock_name,
+    SUM(price) FILTER(WHERE operation = 'Sell') - SUM(price) FILTER(WHERE operation = 'Buy') AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
