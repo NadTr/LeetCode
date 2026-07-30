ab95ff93a7a199164463a3388d0d8c1c33df0e42
Nadine <trauflernadine@gmail.com>
2026-07-30 07:30:47 +0200

    Python / group-anagrams

---
+++ top-interview-150/05-Hashmap/05-group-anagrams.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+
+        for word in strs:
+            key = "".join(sorted(word))
+            anagrams[key].append(word)
+
+        return list(anagrams.values())
+        
